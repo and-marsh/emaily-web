1e92b00f05b10b1d5eaee91a49d72351fb92de06
Andrei Marshalov <andrei.marshalov@emlid.com>
2024-01-07 19:04:56 +0100

    Add user-authentication flow using session cookies

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const express = require("express");
 const mongoose = require('mongoose');
-const keys = require('./config/keys')
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require('./config/keys');
+require('./models/User');
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+// fix for "TypeError: req.session.regenerate is not a function"
+// https://stackoverflow.com/a/75195471/11532910
+app.use(function(request, response, next) {
+    if (request.session && !request.session.regenerate) {
+        request.session.regenerate = (cb) => {
+            cb()
+        }
+    }
+    if (request.session && !request.session.save) {
+        request.session.save = (cb) => {
+            cb()
+        }
+    }
+    next()
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
