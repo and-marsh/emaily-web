64cc8f8156bb7eccbac2f53b7ff917be152bdadf
Andrei Marshalov <andrei.marshalov@emlid.com>
2024-01-18 10:46:24 +0100

    Add fetchUser action

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
 import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
 import ReactDOM from "react-dom/client";
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from 'redux';
+import { thunk } from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
