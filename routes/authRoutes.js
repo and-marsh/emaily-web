c92750ff74688969837602c0c2ef9cc54f00f351
Andrei Marshalov <andrei.marshalov@emlid.com>
2024-02-01 09:43:38 +0100

    Wire up login/logout routing

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,11 +8,17 @@ module.exports = app => {
         })
     );
 
-    app.get('/auth/google/callback', passport.authenticate('google'));
+    app.get(
+        '/auth/google/callback',
+        passport.authenticate('google'),
+        (req, res) => {
+            res.redirect("/surveys");
+        }
+    );
 
     app.get('/api/logout', (req, res) => {
         req.logout(req.user, err => {
-            res.send(req.user)
+            res.redirect("/");
         });
     });
 
