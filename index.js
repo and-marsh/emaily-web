72fb7c80f31bdeb82656d736b620ea4bfad569b0
Andrei Marshalov <andrei.marshalov@emlid.com>
2024-02-25 20:26:32 +0100

    Remove code that was needed for debugging

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cookieSession = require("cookie-session");
 const passport = require("passport");
 const bodyParser = require("body-parser");
 const keys = require('./config/keys');
-const got = import('got');
 require('./models/User');
 require("./models/Survey");
 require("./services/passport");
@@ -43,7 +42,6 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 require("./routes/surveyRoutes")(app);
-require("./routes/webhookRoutes")(app, got);
 
 if(process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
