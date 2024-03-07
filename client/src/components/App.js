0cbf02d583a5d85586902e96402c34249ba8447f
Andrei Marshalov <andrei.marshalov@emlid.com>
2024-03-07 20:43:55 +0100

    Add input form for `/surveys/new` route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,26 +6,24 @@ import * as actions from '../actions'
 import Header from "./Header"
 import Landing from "./Landing";
 import Dashboard from "./Dashboard";
-const SurveyNew = () => <h2>SurveyNew</h2>
+import SurveyNew from "./surveys/SurveyNew";
 
 class App extends Component {
-    componentDidMount () {
+    componentDidMount() {
         console.log(`App Component did mount ${Date.now()}`);
         this.props.fetchUser();
     }
 
     render() {
         return (
-            <div className="container">
-                <BrowserRouter>
-                    <div>
-                        <Header />
-                        <Route exact path="/" component={Landing} />
-                        <Route exact path="/surveys" component={Dashboard} />
-                        <Route exact path="/surveys/new" component={SurveyNew} />
-                    </div>
-                </BrowserRouter>
-            </div>
+            <BrowserRouter>
+                <div className="container">
+                    <Header />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/surveys" component={Dashboard} />
+                    <Route exact path="/surveys/new" component={SurveyNew} />
+                </div>
+            </BrowserRouter>
         );
     }
 };
