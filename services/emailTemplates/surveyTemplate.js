44448a1c3dd5ab0811de5c7e762dce485ad6b2f6
Andrei Marshalov <andrei.marshalov@emlid.com>
2024-03-21 19:38:05 +0100

    Finish SurveyNew form

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -8,7 +8,7 @@ module.exports = (survey) => {
             <div style=\\"text-align: center;\\">
                 <h3>I'd like your input</h3>
                 <p>Please answer the following question:</p>
-                <p>the body of the email</p>
+                <p>${survey.body}</p>
                 <div> <a href=${keys.redirectDomain}/api/surveys/thanks>Yes</a> </div>
                 <div> <a href=${keys.redirectDomain}/api/surveys/thanks>No</a> </div>
             </div>
