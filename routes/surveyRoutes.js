b4687ca1f0eae3797e76a0bc2b9ac344b3c9c674
Andrei Marshalov <andrei.marshalov@emlid.com>
2024-04-14 21:33:31 +0200

    Update Survey on click event

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -4,10 +4,31 @@ const requireCredits = require("../middlewares/requireCredits.js");
 const Survey = mongoose.model("surveys");
 const Mailer = require("../services/Mailer.js");
 const surveyTemplate = require("../services/emailTemplates/surveyTemplate.js");
+const bodyParser = require('body-parser')
 
 module.exports = app => {
     app.get("/api/surveys/thanks", (req, res) => {
         res.send("Thanks for voting!");
+    });
+
+    app.post("/api/survey/webhooks", bodyParser.text(), (req, res) => {
+        const { surveyId, email, choice } = extractClickData(req.body);
+
+        Survey.updateOne(
+            {
+                _id: surveyId,
+                recipients: {
+                    $elemMatch: { email: email, responded: false },
+                },
+            },
+            {
+                $inc: { [choice]: 1 },
+                $set: { 'recipients.$.responded': true },
+                lastResponded: new Date()
+            }
+        ).exec();
+
+        res.send({});
     });
 
     app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
@@ -43,3 +64,18 @@ module.exports = app => {
         }
     });
 };
+
+
+function extractClickData(body) {
+    const parsedBody = JSON.parse(body);
+    const message = JSON.parse(parsedBody.Message);
+
+    const eventType = message.eventType;
+    const email = message.mail.destination[0];
+    const surveyId = message.click.linkTags.surveyId[0];
+    const choice = message.click.linkTags.answer[0];
+
+    if (eventType === "Click" && email && surveyId && choice) {
+        return { surveyId, email, choice };
+    }
+}
