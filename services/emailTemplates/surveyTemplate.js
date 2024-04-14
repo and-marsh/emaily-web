e616a846605d0057007798b6211a7e4dc3341b1c
Andrei Marshalov <andrei.marshalov@emlid.com>
2024-04-14 21:33:12 +0200

    Add tags to response links

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -9,8 +9,8 @@ module.exports = (survey) => {
                 <h3>I'd like your input</h3>
                 <p>Please answer the following question:</p>
                 <p>${survey.body}</p>
-                <div> <a href=${keys.redirectDomain}/api/surveys/thanks>Yes</a> </div>
-                <div> <a href=${keys.redirectDomain}/api/surveys/thanks>No</a> </div>
+                <div> <a ses:tags=\\"surveyId:${survey.id};answer:yes\\"; href=${keys.redirectDomain}/api/surveys/thanks>Yes</a> </div>
+                <div> <a ses:tags=\\"surveyId:${survey.id};answer:no\\"; href=${keys.redirectDomain}/api/surveys/thanks>No</a> </div>
             </div>
         </body>
 
